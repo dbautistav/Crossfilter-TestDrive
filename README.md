e67ea7f10703581202c9702f505960861c331926
David Bautista <dbautistav@gmail.com>
2016-02-06 10:12:10 -0600

    Filters (WIP)

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Testing [Crossfilter](https://github.com/square/crossfilter) following [a tutori
 # Disclaimer
 **WARNING**: displaying the demo could hang your tab, browser or even your entire computer (since the earlier is _really heavy_ because associated data: ~52.1 MB).
 I recommend to save every work in progress at every single computer application which you were working on before checking the demo otherwise you may lose unsaved data.
-I also discourage use mobile data to display this demo and look for a wi-fi connection instead.
+I also discourage use mobile data to display this demo and look for a Wi-Fi connection instead.
 
-If once you have **read and understood** this disclaimer you still want to check the demo [enter here](https://dbautistav.github.io/Crossfilter-TestDrive).
+If once you have **read and understood** this disclaimer you still want to check the demo, [enter here](https://dbautistav.github.io/Crossfilter-TestDrive).
--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 "use strict";
 
 (function () {
-    //var dataUrl = "./data/11.json";
-    var dataUrl = "https://raw.githubusercontent.com/dbautistav/datahub/gh-pages/ecobici/2015/11.json";
+    var dataUrl = "./data/11.json";
+    //var dataUrl = "https://raw.githubusercontent.com/dbautistav/datahub/gh-pages/ecobici/2015/11.json";
 
-    var dataset, events;
+    var dataset, observations;
 
     function doCrossfilter() {
         ////  [795614]:
@@ -20,17 +20,68 @@
         ////    Hora_Retiro: "00:00:01.623000"
         ////};
 
-        events = crossfilter(dataset);
+        observations = crossfilter(dataset);
+        //console.log("observations", observations);
 
-        $("#container").html("<h2>Items loaded: " + events.size() + "</h2>");
+        // ~~~~
+        $("#container").html("<h2>Items loaded: " + observations.size() + "</h2><p>Check the dev-tools console.</p>");
 
-        events.dispose();
+        var byBike = observations.dimension(function (o) {
+            return o.Bici;
+        });
+
+
+        var byBikeGroup = byBike.group();
+
+        //// Prints every 'Bici' id and number of occurrences:
+        //byBikeGroup.top(Infinity).forEach(function (o, i) {
+        //    console.log(o.key + ": " + o.value);
+        //});
+
+        byBike.filterExact("0331");
+        byBike.top(Infinity).forEach(function (o, i) {
+            console.log(o.Edad_Usuario + ". " + o.Genero_Usuario);
+        });
+
+        byBike.filterAll();
+
+
+        var byAge = observations.dimension(function (o) {
+            return o.Edad_Usuario;
+        });
+        console.log("Total # of users: " +
+            byAge.top(Infinity).length);
+
+        var t0 = 63, tf = 65;
+        byAge.filter([t0, tf]);
+        console.log("# of users between " + t0 + " yo and " + tf + " yo (both included): " +
+            byAge.top(Infinity).length);
+
+
+        //// Prints every 'Bici' id and number of occurrences (affected by previous 'byAge.filter'):
+        //byBikeGroup.top(Infinity).forEach(function (o, i) {
+        //    console.log(o.key + ": " + o.value);
+        //});
+
+
+        // ~~~~
+        var byAgeGroup = byAge.group();
+        byAgeGroup.top(Infinity).forEach(function (o, i) {
+            console.log(o.key + ": " + o.value);
+        });
+
+
+        // This frees up space.
+        byBikeGroup.dispose();
+        byBike.dispose();
+        byAgeGroup.dispose();
+        byAge.dispose();
 
     }
 
 
     function fillDataset(response) {
-        console.log("response", response, response.length);
+        //console.log("response", response, response.length);
         dataset = response;
         doCrossfilter();
 
